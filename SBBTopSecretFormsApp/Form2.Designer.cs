220706fccca89a661329260a230534fb73d4b79b
Blockkanone <hendrik.ott@bluewin.ch>
2020-11-26 15:47:36 +0100

    Forms zwei "Verbindungen" erstellt und Modeliert

---
+++ SBBTopSecretFormsApp/Form2.Designer.cs
@@ -0,0 +1,313 @@
+﻿
+namespace SBBTopSecretFormsApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.arrivalStation = new System.Windows.Forms.TextBox();
+            this.depatureStation = new System.Windows.Forms.TextBox();
+            this.arrivalLabel = new System.Windows.Forms.Label();
+            this.depatureLabel = new System.Windows.Forms.Label();
+            this.depatureTimeLabel = new System.Windows.Forms.Label();
+            this.depatureDateLabel = new System.Windows.Forms.Label();
+            this.depatureTimetbx = new System.Windows.Forms.TextBox();
+            this.depatureDatetbx = new System.Windows.Forms.TextBox();
+            this.arrivalTimeLabel = new System.Windows.Forms.Label();
+            this.arrivalDateLabel = new System.Windows.Forms.Label();
+            this.arrivalTimetbx = new System.Windows.Forms.TextBox();
+            this.arrivalDatetbx = new System.Windows.Forms.TextBox();
+            this.depatureTypetbx = new System.Windows.Forms.TextBox();
+            this.depatureLinetbx = new System.Windows.Forms.TextBox();
+            this.depatureStationNrtbx = new System.Windows.Forms.TextBox();
+            this.depatureTypeLabel = new System.Windows.Forms.Label();
+            this.depatureLineLabel = new System.Windows.Forms.Label();
+            this.depatureStationNrLabel = new System.Windows.Forms.Label();
+            this.arrivalStationNrLabel = new System.Windows.Forms.Label();
+            this.arrivalLineLabel = new System.Windows.Forms.Label();
+            this.arrivalTypeLabel = new System.Windows.Forms.Label();
+            this.arrivalStationNrtbx = new System.Windows.Forms.TextBox();
+            this.arrivalLinetbx = new System.Windows.Forms.TextBox();
+            this.arrivalTypetbx = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // arrivalStation
+            // 
+            this.arrivalStation.Location = new System.Drawing.Point(363, 41);
+            this.arrivalStation.Name = "arrivalStation";
+            this.arrivalStation.Size = new System.Drawing.Size(100, 20);
+            this.arrivalStation.TabIndex = 7;
+            // 
+            // depatureStation
+            // 
+            this.depatureStation.Location = new System.Drawing.Point(114, 41);
+            this.depatureStation.Name = "depatureStation";
+            this.depatureStation.Size = new System.Drawing.Size(100, 20);
+            this.depatureStation.TabIndex = 6;
+            // 
+            // arrivalLabel
+            // 
+            this.arrivalLabel.AutoSize = true;
+            this.arrivalLabel.Location = new System.Drawing.Point(360, 24);
+            this.arrivalLabel.Name = "arrivalLabel";
+            this.arrivalLabel.Size = new System.Drawing.Size(21, 13);
+            this.arrivalLabel.TabIndex = 5;
+            this.arrivalLabel.Text = "Bis";
+            // 
+            // depatureLabel
+            // 
+            this.depatureLabel.AutoSize = true;
+            this.depatureLabel.Location = new System.Drawing.Point(111, 24);
+            this.depatureLabel.Name = "depatureLabel";
+            this.depatureLabel.Size = new System.Drawing.Size(26, 13);
+            this.depatureLabel.TabIndex = 4;
+            this.depatureLabel.Text = "Von";
+            // 
+            // depatureTimeLabel
+            // 
+            this.depatureTimeLabel.AutoSize = true;
+            this.depatureTimeLabel.Location = new System.Drawing.Point(220, 72);
+            this.depatureTimeLabel.Name = "depatureTimeLabel";
+            this.depatureTimeLabel.Size = new System.Drawing.Size(40, 13);
+            this.depatureTimeLabel.TabIndex = 11;
+            this.depatureTimeLabel.Text = "Uhrzeit";
+            // 
+            // depatureDateLabel
+            // 
+            this.depatureDateLabel.AutoSize = true;
+            this.depatureDateLabel.Location = new System.Drawing.Point(114, 72);
+            this.depatureDateLabel.Name = "depatureDateLabel";
+            this.depatureDateLabel.Size = new System.Drawing.Size(38, 13);
+            this.depatureDateLabel.TabIndex = 10;
+            this.depatureDateLabel.Text = "Datum";
+            // 
+            // depatureTimetbx
+            // 
+            this.depatureTimetbx.Location = new System.Drawing.Point(223, 88);
+            this.depatureTimetbx.Name = "depatureTimetbx";
+            this.depatureTimetbx.Size = new System.Drawing.Size(100, 20);
+            this.depatureTimetbx.TabIndex = 9;
+            // 
+            // depatureDatetbx
+            // 
+            this.depatureDatetbx.Location = new System.Drawing.Point(114, 88);
+            this.depatureDatetbx.Name = "depatureDatetbx";
+            this.depatureDatetbx.Size = new System.Drawing.Size(100, 20);
+            this.depatureDatetbx.TabIndex = 8;
+            // 
+            // arrivalTimeLabel
+            // 
+            this.arrivalTimeLabel.AutoSize = true;
+            this.arrivalTimeLabel.Location = new System.Drawing.Point(465, 72);
+            this.arrivalTimeLabel.Name = "arrivalTimeLabel";
+            this.arrivalTimeLabel.Size = new System.Drawing.Size(40, 13);
+            this.arrivalTimeLabel.TabIndex = 15;
+            this.arrivalTimeLabel.Text = "Uhrzeit";
+            // 
+            // arrivalDateLabel
+            // 
+            this.arrivalDateLabel.AutoSize = true;
+            this.arrivalDateLabel.Location = new System.Drawing.Point(359, 72);
+            this.arrivalDateLabel.Name = "arrivalDateLabel";
+            this.arrivalDateLabel.Size = new System.Drawing.Size(38, 13);
+            this.arrivalDateLabel.TabIndex = 14;
+            this.arrivalDateLabel.Text = "Datum";
+            // 
+            // arrivalTimetbx
+            // 
+            this.arrivalTimetbx.Location = new System.Drawing.Point(468, 88);
+            this.arrivalTimetbx.Name = "arrivalTimetbx";
+            this.arrivalTimetbx.Size = new System.Drawing.Size(100, 20);
+            this.arrivalTimetbx.TabIndex = 13;
+            // 
+            // arrivalDatetbx
+            // 
+            this.arrivalDatetbx.Location = new System.Drawing.Point(362, 88);
+            this.arrivalDatetbx.Name = "arrivalDatetbx";
+            this.arrivalDatetbx.Size = new System.Drawing.Size(100, 20);
+            this.arrivalDatetbx.TabIndex = 12;
+            // 
+            // depatureTypetbx
+            // 
+            this.depatureTypetbx.Location = new System.Drawing.Point(114, 137);
+            this.depatureTypetbx.Name = "depatureTypetbx";
+            this.depatureTypetbx.Size = new System.Drawing.Size(38, 20);
+            this.depatureTypetbx.TabIndex = 16;
+            // 
+            // depatureLinetbx
+            // 
+            this.depatureLinetbx.Location = new System.Drawing.Point(158, 137);
+            this.depatureLinetbx.Name = "depatureLinetbx";
+            this.depatureLinetbx.Size = new System.Drawing.Size(38, 20);
+            this.depatureLinetbx.TabIndex = 17;
+            // 
+            // depatureStationNrtbx
+            // 
+            this.depatureStationNrtbx.Location = new System.Drawing.Point(202, 137);
+            this.depatureStationNrtbx.Name = "depatureStationNrtbx";
+            this.depatureStationNrtbx.Size = new System.Drawing.Size(38, 20);
+            this.depatureStationNrtbx.TabIndex = 18;
+            // 
+            // depatureTypeLabel
+            // 
+            this.depatureTypeLabel.AutoSize = true;
+            this.depatureTypeLabel.Location = new System.Drawing.Point(111, 121);
+            this.depatureTypeLabel.Name = "depatureTypeLabel";
+            this.depatureTypeLabel.Size = new System.Drawing.Size(25, 13);
+            this.depatureTypeLabel.TabIndex = 19;
+            this.depatureTypeLabel.Text = "Typ";
+            // 
+            // depatureLineLabel
+            // 
+            this.depatureLineLabel.AutoSize = true;
+            this.depatureLineLabel.Location = new System.Drawing.Point(155, 121);
+            this.depatureLineLabel.Name = "depatureLineLabel";
+            this.depatureLineLabel.Size = new System.Drawing.Size(29, 13);
+            this.depatureLineLabel.TabIndex = 20;
+            this.depatureLineLabel.Text = "Linie";
+            // 
+            // depatureStationNrLabel
+            // 
+            this.depatureStationNrLabel.AutoSize = true;
+            this.depatureStationNrLabel.Location = new System.Drawing.Point(199, 121);
+            this.depatureStationNrLabel.Name = "depatureStationNrLabel";
+            this.depatureStationNrLabel.Size = new System.Drawing.Size(30, 13);
+            this.depatureStationNrLabel.TabIndex = 21;
+            this.depatureStationNrLabel.Text = "Gleis";
+            // 
+            // arrivalStationNrLabel
+            // 
+            this.arrivalStationNrLabel.AutoSize = true;
+            this.arrivalStationNrLabel.Location = new System.Drawing.Point(448, 121);
+            this.arrivalStationNrLabel.Name = "arrivalStationNrLabel";
+            this.arrivalStationNrLabel.Size = new System.Drawing.Size(30, 13);
+            this.arrivalStationNrLabel.TabIndex = 27;
+            this.arrivalStationNrLabel.Text = "Gleis";
+            // 
+            // arrivalLineLabel
+            // 
+            this.arrivalLineLabel.AutoSize = true;
+            this.arrivalLineLabel.Location = new System.Drawing.Point(404, 121);
+            this.arrivalLineLabel.Name = "arrivalLineLabel";
+            this.arrivalLineLabel.Size = new System.Drawing.Size(29, 13);
+            this.arrivalLineLabel.TabIndex = 26;
+            this.arrivalLineLabel.Text = "Linie";
+            // 
+            // arrivalTypeLabel
+            // 
+            this.arrivalTypeLabel.AutoSize = true;
+            this.arrivalTypeLabel.Location = new System.Drawing.Point(360, 121);
+            this.arrivalTypeLabel.Name = "arrivalTypeLabel";
+            this.arrivalTypeLabel.Size = new System.Drawing.Size(25, 13);
+            this.arrivalTypeLabel.TabIndex = 25;
+            this.arrivalTypeLabel.Text = "Typ";
+            // 
+            // arrivalStationNrtbx
+            // 
+            this.arrivalStationNrtbx.Location = new System.Drawing.Point(451, 137);
+            this.arrivalStationNrtbx.Name = "arrivalStationNrtbx";
+            this.arrivalStationNrtbx.Size = new System.Drawing.Size(38, 20);
+            this.arrivalStationNrtbx.TabIndex = 24;
+            // 
+            // arrivalLinetbx
+            // 
+            this.arrivalLinetbx.Location = new System.Drawing.Point(407, 137);
+            this.arrivalLinetbx.Name = "arrivalLinetbx";
+            this.arrivalLinetbx.Size = new System.Drawing.Size(38, 20);
+            this.arrivalLinetbx.TabIndex = 23;
+            // 
+            // arrivalTypetbx
+            // 
+            this.arrivalTypetbx.Location = new System.Drawing.Point(363, 137);
+            this.arrivalTypetbx.Name = "arrivalTypetbx";
+            this.arrivalTypetbx.Size = new System.Drawing.Size(38, 20);
+            this.arrivalTypetbx.TabIndex = 22;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(678, 393);
+            this.Controls.Add(this.arrivalStationNrLabel);
+            this.Controls.Add(this.arrivalLineLabel);
+            this.Controls.Add(this.arrivalTypeLabel);
+            this.Controls.Add(this.arrivalStationNrtbx);
+            this.Controls.Add(this.arrivalLinetbx);
+            this.Controls.Add(this.arrivalTypetbx);
+            this.Controls.Add(this.depatureStationNrLabel);
+            this.Controls.Add(this.depatureLineLabel);
+            this.Controls.Add(this.depatureTypeLabel);
+            this.Controls.Add(this.depatureStationNrtbx);
+            this.Controls.Add(this.depatureLinetbx);
+            this.Controls.Add(this.depatureTypetbx);
+            this.Controls.Add(this.arrivalTimeLabel);
+            this.Controls.Add(this.arrivalDateLabel);
+            this.Controls.Add(this.arrivalTimetbx);
+            this.Controls.Add(this.arrivalDatetbx);
+            this.Controls.Add(this.depatureTimeLabel);
+            this.Controls.Add(this.depatureDateLabel);
+            this.Controls.Add(this.depatureTimetbx);
+            this.Controls.Add(this.depatureDatetbx);
+            this.Controls.Add(this.arrivalStation);
+            this.Controls.Add(this.depatureStation);
+            this.Controls.Add(this.arrivalLabel);
+            this.Controls.Add(this.depatureLabel);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox arrivalStation;
+        private System.Windows.Forms.TextBox depatureStation;
+        private System.Windows.Forms.Label arrivalLabel;
+        private System.Windows.Forms.Label depatureLabel;
+        private System.Windows.Forms.Label depatureTimeLabel;
+        private System.Windows.Forms.Label depatureDateLabel;
+        private System.Windows.Forms.TextBox depatureTimetbx;
+        private System.Windows.Forms.TextBox depatureDatetbx;
+        private System.Windows.Forms.Label arrivalTimeLabel;
+        private System.Windows.Forms.Label arrivalDateLabel;
+        private System.Windows.Forms.TextBox arrivalTimetbx;
+        private System.Windows.Forms.TextBox arrivalDatetbx;
+        private System.Windows.Forms.TextBox depatureTypetbx;
+        private System.Windows.Forms.TextBox depatureLinetbx;
+        private System.Windows.Forms.TextBox depatureStationNrtbx;
+        private System.Windows.Forms.Label depatureTypeLabel;
+        private System.Windows.Forms.Label depatureLineLabel;
+        private System.Windows.Forms.Label depatureStationNrLabel;
+        private System.Windows.Forms.Label arrivalStationNrLabel;
+        private System.Windows.Forms.Label arrivalLineLabel;
+        private System.Windows.Forms.Label arrivalTypeLabel;
+        private System.Windows.Forms.TextBox arrivalStationNrtbx;
+        private System.Windows.Forms.TextBox arrivalLinetbx;
+        private System.Windows.Forms.TextBox arrivalTypetbx;
+    }
+}
---
+++ SBBTopSecretFormsApp/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SBBTopSecretFormsApp
+{
+    public partial class Form2 : Form
+    {
+        public Form2(/*Form form1, Form form2*/)
+        {
+            InitializeComponent();
+        }
+
+        /*public class forms
+        { 
+        public Form form1 { get; }
+        public Form form2 { get;  }
+            
+        }
+        forms froms = new forms();
+
+        private void Form2_Activated(object sender, EventArgs e, Form form1)
+        {
+            form1.Close();
+        }*/
+    }
+}
