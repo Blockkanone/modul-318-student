627e3d5a1220cad16db4289aa2ed7eb344f9011d
Blockkanone <hendrik.ott@bluewin.ch>
2020-11-26 15:47:01 +0100

    Stationen suchen

--- SBBTopSecretFormsApp/Form1.cs
+++ SBBTopSecretFormsApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,30 @@ namespace SBBTopSecretFormsApp
 {
     public partial class Form1 : Form
     {
+        ITransport _transport = new Transport();
         public Form1()
         {
             InitializeComponent();
+            // form2 = new Form2(/*this, form2 */);
+        }
+        Form form2 = new Form2();
+
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            var result = _transport.GetStations(query: depatureStation.Text);
+            try { 
+            MessageBox.Show(result.StationList[0].Name);
+
+            }
+            catch(ArgumentOutOfRangeException) 
+            { 
+                MessageBox.Show("Es wurde keine Station gefunden");
+            }
+
+            
+            form2.Show();
+            form2.Focus();
         }
     }
 }
