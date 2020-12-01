7655082147cfbd7f24b85b1f8d8e6c49f02a4437
Blockkanone <hendrik.ott@bluewin.ch>
2020-12-01 13:26:46 +0100

    Safecommit

--- SBBTopSecretFormsApp/Form1.cs
+++ SBBTopSecretFormsApp/Form1.cs
@@ -17,21 +17,41 @@ namespace SBBTopSecretFormsApp
         public Form1()
         {
             InitializeComponent();
-            // form2 = new Form2(/*this, form2 */);
+            dateSearch.Text = DateTime.Now.ToShortDateString();
+            timeSearch.Text = DateTime.Now.ToShortTimeString();
         }
-        Form form2 = new Form2();
 
 
         private void searchButton_Click(object sender, EventArgs e)
         {
+            
             if (depatureStation.Text != "" && arrivalStation.Text != "") { 
             var depatureTextResult = _transport.GetStations(query: depatureStation.Text);
-            var arrivalTextResult = _transport.GetStations(query: depatureStation.Text);
+            var arrivalTextResult = _transport.GetStations(query: arrivalStation.Text);
 
                 try
                 {
-               string depatureTextTesting = depatureTextResult.StationList[0].Name;
-                    string arrivalTextTesting = arrivalTextResult.StationList[0].Name;
+               string depatureText = depatureTextResult.StationList[0].Name;
+                    string arrivalText = arrivalTextResult.StationList[0].Name;
+
+                    DateTime dateSearchConnection = DateTime.Parse(dateSearch.Text);
+                    string YearSearch = dateSearchConnection.Year.ToString();
+                    string MonthSearch = dateSearchConnection.Month.ToString();
+                    string DaySearch = dateSearchConnection.Day.ToString();
+
+                    if (MonthSearch == "1" || MonthSearch == "2" || MonthSearch == "3" || MonthSearch == "4" || MonthSearch == "5" || MonthSearch == "6" || MonthSearch == "7" || MonthSearch == "8" || MonthSearch == "9" )
+                    {
+                        MonthSearch = "0" + MonthSearch;
+                    }
+
+                    if (DaySearch == "1" || DaySearch == "2" || DaySearch == "3" || DaySearch == "4" || DaySearch == "5" || DaySearch == "6" || DaySearch == "7" || DaySearch == "8" || DaySearch == "9")
+                    {
+                        DaySearch = "0" + DaySearch;
+                    }
+
+                    string DateTimeSearchConnection = YearSearch + "-" + MonthSearch + "-" + DaySearch;
+                    var connectionResult = _transport.GetConnections(fromStation: depatureStation.Text, toStattion: arrivalStation.Text/*, date: /*DateTimeSearchConnection"2020-12-01", time: /*timeSearch.Text"13:22"*/);
+                    Form form2 = new Form2(connectionResult);
                     form2.Show();
                 form2.Focus();
             }
--- SBBTopSecretFormsApp/Form2.cs
+++ SBBTopSecretFormsApp/Form2.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using SwissTransport;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,24 +12,31 @@ using System.Windows.Forms;
 
 namespace SBBTopSecretFormsApp
 {
+
     public partial class Form2 : Form
     {
-        public Form2(/*Form form1, Form form2*/)
+        ITransport _transport = new Transport();
+        public Form2(Connections connectionResult)
         {
             InitializeComponent();
+            var depatureDateTime = DateTime.Parse(connectionResult.ConnectionList[0].From.Departure);
+            var arrivalDateTime = DateTime.Parse(connectionResult.ConnectionList[0].To.Arrival);
+
+            depatureStation.Text = Convert.ToString(connectionResult.ConnectionList[0].From.Station.Name);
+            arrivalStation.Text = Convert.ToString(connectionResult.ConnectionList[0].To.Station.Name);
+
+            depatureDatetbx.Text = depatureDateTime.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+            arrivalDatetbx.Text = arrivalDateTime.ToString("dd.MM.yyyy", CultureInfo.InvariantCulture);
+            depatureTimetbx.Text = depatureDateTime.ToString("HH:mm", CultureInfo.InvariantCulture);
+            arrivalTimetbx.Text = arrivalDateTime.ToString("HH:mm", CultureInfo.InvariantCulture);
+
+            depatureStationNrtbx.Text = Convert.ToString(connectionResult.ConnectionList[0].From.Platform);
+            arrivalStationNrtbx.Text = Convert.ToString(connectionResult.ConnectionList[0].To.Platform);
         }
 
-        /*public class forms
-        { 
-        public Form form1 { get; }
-        public Form form2 { get;  }
-            
-        }
-        forms froms = new forms();
 
-        private void Form2_Activated(object sender, EventArgs e, Form form1)
-        {
-            form1.Close();
-        }*/
+
+
+      
     }
 }
